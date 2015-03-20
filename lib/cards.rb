0d9e834d938eed448e5a6e69809da64c4479246d
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-19 23:31:51 -0400

    ocd yo

--- lib/cards.rb
+++ lib/cards.rb
@@ -2,14 +2,16 @@ require "cards/version"
 
 module Cards
   class Card
-    attr_accessor :text, :url
-    def initialize(text: "", url: "")
+    attr_accessor :text, :url, :image
+    def initialize(image: "", text: "", url: "")
+      @image = image if image.length > 0
       @text = text if text.length > 0
       @url = url if url.length > 0
     end
 
     def components
       r = []
+      r << :image if image
       r << :text if text
       r << :url if url
       return r
